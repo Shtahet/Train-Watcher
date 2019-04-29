e21f886f03d318460c056dad069f3c03e26ce32e
Shtahet <derien001@gmal.com>
2019-04-29 14:21:54 +0300

    Add project files.

---
+++ TrainWatcher.Core/Model/Station.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainWatcher.Core.Model
+{
+	public class Station
+	{
+		private static Station __kherson;
+		private static Station __kyiv;
+		public string Title { get; set; }
+		public string Region { get; set; }
+		public int Code { get; set; }
+
+		public static Station GetKherson()
+		{
+			if (__kherson == null)
+			{
+				__kherson = new Station
+				{
+					Title = "Херсон",
+					Code = 2208530
+				};
+			}
+			return __kherson;
+		}
+
+		public static Station GetKyiv()
+		{
+			if (__kyiv == null)
+			{
+				__kyiv = new Station
+				{
+					Title = "Киев",
+					Code = 2200001
+				};
+			}
+			return __kyiv;
+		}
+
+		public static Station FromJson(string jsonObject)
+		{
+			JsonValue.Parse
+		}
+	}
+}
