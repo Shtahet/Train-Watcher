b75fd1e51448347a37bb84ad8bd703b3476069d1
Shtahet <derien001@gmal.com>
2019-05-03 01:56:54 +0300

    Add uTest project with simple test

---
+++ UnitTests/ModelTest.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrainWatcher.Core.Model;
+
+namespace TrainWatcher.UnitTests
+{
+	[TestFixture]
+	class ModelTest
+	{
+		[Test]
+		public void DeserializeStationFromJson()
+		{
+			//Arrange
+			string jsonData = "{\"title\":\"Киевская Русановка\",\"region\":null,\"value\":2201180}";
+
+			//Act
+			Station obj = JsonConvert.DeserializeObject<Station>(jsonData);
+
+			//Assert
+			Assert.Multiple(() =>
+			{
+				Assert.That(obj.Region, Is.Null);
+				Assert.That(obj.Title, Is.EqualTo("Киевская Русановка"));
+				Assert.That(obj.Code, Is.EqualTo(2201180));
+			});
+		}
+	}
+}
---
+++ UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f0e78883-f352-4c77-acb5-796c59031e39")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
