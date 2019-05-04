a97497fd0514f016d61344b8f2cdae54a3222da1
Shtahet <derien001@gmal.com>
2019-05-05 01:48:50 +0300

    Dev. Add model for Wagon with test

---
+++ TrainWatcher.Core/Model/Wagon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainWatcher.Core.Model
+{
+	public class Wagon
+	{
+		public string Id { get; set; }
+		public string Title { get; set; }
+		public string Letter { get; set; }
+		public uint Places { get; set; }
+	}
+}
--- UnitTests/ModelTest.cs
+++ UnitTests/ModelTest.cs
@@ -47,16 +47,36 @@ namespace TrainWatcher.UnitTests
 			RoutePoint obj = JsonConvert.DeserializeObject<RoutePoint>(jsonData, settings);
 
 			//Assert
-			Assert.That(obj.Code, Is.EqualTo(2208530));
-			Assert.That(obj.Station, Is.EqualTo("Херсон_station"));
-			Assert.That(obj.TrainName, Is.EqualTo("Херсон_train"));
-			Assert.That(obj.Date, Is.EqualTo(new DateTime(2019, 06, 01)));
-			Assert.That(obj.Time, Is.EqualTo(assertionTime));
-			Assert.That(obj.SortTime, Is.EqualTo(1559393520));
-			Assert.That(obj.SrcDate, Is.EqualTo(new DateTime(2019, 6, 1)));
+			Assert.Multiple(() =>
+			{
+				Assert.That(obj.Code, Is.EqualTo(2208530));
+				Assert.That(obj.Station, Is.EqualTo("Херсон_station"));
+				Assert.That(obj.TrainName, Is.EqualTo("Херсон_train"));
+				Assert.That(obj.Date, Is.EqualTo(new DateTime(2019, 06, 01)));
+				Assert.That(obj.Time, Is.EqualTo(assertionTime));
+				Assert.That(obj.SortTime, Is.EqualTo(1559393520));
+				Assert.That(obj.SrcDate, Is.EqualTo(new DateTime(2019, 6, 1)));
+			});
 		}
 
+		[Test]
+		public void DeserializeWagonGenerlaInforFromJson()
+		{
+			//Arrange
+			string jsonData = "{\"id\":\"С1\",\"title\":\"Сидячий первого класса\",\"letter\":\"С1\",\"places\":8}";
 
+			//Act
+			Wagon obj = JsonConvert.DeserializeObject<Wagon>(jsonData);
+
+			//Assert
+			Assert.Multiple(() =>
+			{
+				Assert.That(obj.Id, Is.EqualTo("С1"));
+				Assert.That(obj.Title, Is.EqualTo("Сидячий первого класса"));
+				Assert.That(obj.Letter, Is.EqualTo("С1"));
+				Assert.That(obj.Places, Is.EqualTo(8));
+			});
+		}
 		//{"data":{"list":[{"num":"765О","category":2,"isTransformer":0,"travelTime":"7:22","from":{"code":"2208530","station":"Херсон","stationTrain":"Херсон","date":"суббота, 01.06.2019","time":"15:52","sortTime":1559393520,"srcDate":"2019-06-01"},"to":{"code":"2200001","station":"Киев-Пассажирский","stationTrain":"Киев-Пассажирский","date":"суббота, 01.06.2019","time":"23:14","sortTime":1559420040},"types":[{"id":"С1","title":"Сидячий первого класса","letter":"С1","places":8},{"id":"С2","title":"Сидячий второго класса","letter":"С2","places":72}],"child":{"minDate":"2005-06-02","maxDate":"2019-05-02"},"allowStudent":1,"allowBooking":1,"isCis":0,"isEurope":0,"allowPrivilege":0,"disabledPrivilegeByDate":1},{"num":"765К","category":0,"isTransformer":0,"travelTime":"7:22","from":,"to":{"code":"2200001","station":"Киев-Пассажирский","stationTrain":"Киев-Пассажирский","date":"суббота, 01.06.2019","time":"23:14","sortTime":1559420040},"types":[],"child":{"minDate":"2005-06-02","maxDate":"2019-05-02"},"allowStudent":1,"allowBooking":1,"isCis":0,"isEurope":0,"allowPrivilege":0,"disabledPrivilegeByDate":1},{"num":"102Ш","category":0,"isTransformer":0,"travelTime":"11:27","from":{"code":"2208530","station":"Херсон","stationTrain":"Херсон","date":"суббота, 01.06.2019","time":"19:29","sortTime":1559406540,"srcDate":"2019-06-01"},"to":{"code":"2200001","station":"Киев-Пассажирский","stationTrain":"Киев-Пассажирский","date":"воскресенье, 02.06.2019","time":"06:56","sortTime":1559447760},"types":[],"child":{"minDate":"2005-06-02","maxDate":"2019-05-02"},"allowStudent":1,"allowBooking":1,"isCis":0,"isEurope":0,"allowPrivilege":0,"disabledPrivilegeByDate":1}]}}
 	}
 }
