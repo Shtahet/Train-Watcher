13cc786eea97615010f5f14c846e71eea78589a8
Shtahet <derien001@gmal.com>
2019-05-14 09:21:38 +0300

    Dev. Add Route model for json with tests

--- TrainWatcher.Core/Model/Route.cs
+++ TrainWatcher.Core/Model/Route.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,35 @@ using System.Threading.Tasks;
 
 namespace TrainWatcher.Core.Model
 {
+	public enum CategoryType
+	{
+		Fast = 0,
+		InterCity = 2
+	}
+
 	public class Route
 	{
+		[JsonProperty("num")]
+		public string Number { get; set; }
+		public CategoryType Category { get; set; }
+		public bool IsTransformer { get; set; }
+		public TimeSpan TravelTime { get; set; }
+		public RoutePoint From { get; set; }
+		public RoutePoint To { get; set; }
+		[JsonProperty("types")]
+		public List<Wagon> Wagons { get; set; }
+		public ChildRouteInfo Child { get; set; }
+		public bool AllowStudent { get; set; }
+		public bool AllowBooking { get; set; }
+		public bool isCis { get; set; }
+		public bool isEurope { get; set; }
+		public bool AllowPrivilege { get; set; }
+		public bool DisabledPrivilegeByDate { get; set; }
+	}
+
+	public class ChildRouteInfo
+	{
+		public DateTime MinDate { get; set; }
+		public DateTime MaxDate { get; set; }
 	}
 }
--- UnitTests/ModelTest.cs
+++ UnitTests/ModelTest.cs
@@ -77,6 +77,34 @@ namespace TrainWatcher.UnitTests
 				Assert.That(obj.Places, Is.EqualTo(8));
 			});
 		}
+
+		[Test]
+		public void DeserializeRouteFromJson()
+		{
+			//Arrange
+			string jsonData = "{\"num\":\"765О\",\"category\":2,\"isTransformer\":0,\"travelTime\":\"7:22\",\"from\":{\"code\":\"2208530\",\"station\":\"Херсон\",\"stationTrain\":\"Херсон\",\"date\":\"суббота, 01.06.2019\",\"time\":\"15:52\",\"sortTime\":1559393520,\"srcDate\":\"2019-06-01\"},\"to\":{\"code\":\"2200001\",\"station\":\"Киев-Пассажирский\",\"stationTrain\":\"Киев-Пассажирский\",\"date\":\"суббота, 01.06.2019\",\"time\":\"23:14\",\"sortTime\":1559420040},\"types\":[{\"id\":\"С1\",\"title\":\"Сидячий первого класса\",\"letter\":\"С1\",\"places\":8},{\"id\":\"С2\",\"title\":\"Сидячий второго класса\",\"letter\":\"С2\",\"places\":72}],\"child\":{\"minDate\":\"2005-06-02\",\"maxDate\":\"2019-05-02\"},\"allowStudent\":1,\"allowBooking\":1,\"isCis\":0,\"isEurope\":0,\"allowPrivilege\":0,\"disabledPrivilegeByDate\":1}";
+			JsonSerializerSettings settings = new JsonSerializerSettings();
+			settings.Converters.Add(MultiDateConverter.DefaultConverter);
+
+			//Act
+			Route obj = JsonConvert.DeserializeObject<Route>(jsonData, settings);
+
+			//Assert
+			Assert.Multiple(() =>
+			{
+				Assert.That(obj.Number, Is.EqualTo("765О"));
+				Assert.That(obj.Category, Is.EqualTo(CategoryType.InterCity));
+				Assert.That(obj.TravelTime, Is.EqualTo(new TimeSpan(7, 22, 0)));
+				Assert.That(obj.Child.MinDate, Is.EqualTo(new DateTime(2005, 6, 2)));
+				Assert.That(obj.Child.MaxDate, Is.EqualTo(new DateTime(2019, 5, 2)));
+				Assert.That(obj.AllowStudent, Is.True);
+				Assert.That(obj.AllowBooking, Is.True);
+				Assert.That(obj.isCis, Is.False);
+				Assert.That(obj.isEurope, Is.False);
+				Assert.That(obj.AllowPrivilege, Is.False);
+				Assert.That(obj.DisabledPrivilegeByDate, Is.True);
+			});
+					}
 		//{"data":{"list":[{"num":"765О","category":2,"isTransformer":0,"travelTime":"7:22","from":{"code":"2208530","station":"Херсон","stationTrain":"Херсон","date":"суббота, 01.06.2019","time":"15:52","sortTime":1559393520,"srcDate":"2019-06-01"},"to":{"code":"2200001","station":"Киев-Пассажирский","stationTrain":"Киев-Пассажирский","date":"суббота, 01.06.2019","time":"23:14","sortTime":1559420040},"types":[{"id":"С1","title":"Сидячий первого класса","letter":"С1","places":8},{"id":"С2","title":"Сидячий второго класса","letter":"С2","places":72}],"child":{"minDate":"2005-06-02","maxDate":"2019-05-02"},"allowStudent":1,"allowBooking":1,"isCis":0,"isEurope":0,"allowPrivilege":0,"disabledPrivilegeByDate":1},{"num":"765К","category":0,"isTransformer":0,"travelTime":"7:22","from":,"to":{"code":"2200001","station":"Киев-Пассажирский","stationTrain":"Киев-Пассажирский","date":"суббота, 01.06.2019","time":"23:14","sortTime":1559420040},"types":[],"child":{"minDate":"2005-06-02","maxDate":"2019-05-02"},"allowStudent":1,"allowBooking":1,"isCis":0,"isEurope":0,"allowPrivilege":0,"disabledPrivilegeByDate":1},{"num":"102Ш","category":0,"isTransformer":0,"travelTime":"11:27","from":{"code":"2208530","station":"Херсон","stationTrain":"Херсон","date":"суббота, 01.06.2019","time":"19:29","sortTime":1559406540,"srcDate":"2019-06-01"},"to":{"code":"2200001","station":"Киев-Пассажирский","stationTrain":"Киев-Пассажирский","date":"воскресенье, 02.06.2019","time":"06:56","sortTime":1559447760},"types":[],"child":{"minDate":"2005-06-02","maxDate":"2019-05-02"},"allowStudent":1,"allowBooking":1,"isCis":0,"isEurope":0,"allowPrivilege":0,"disabledPrivilegeByDate":1}]}}
 	}
 }
